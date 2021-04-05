81bc90cb3b2949464ebc23e7f6f4fd9492bd82ca
Min Chang <minchangmhc@gmail.com>
2021-04-05 14:18:28 -0700

    Add form and UI logic
    Co-authored-by: Carlos Mendez <carlosmendez86@gmail.com>
    Co-authored-by: Min Chang <minchangmhc@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+// -----Utility Logic-----
+
+// -----Business Logic-----
+
+// -----User Interface Logic-----
+$(document).ready(function () {
+  let item = $("#todoItem").val();
+  $("form#todoForm").submit(function (event) {
+    $("#listItem").append();
+    event.preventDefault();
+
+
+  })
+})
